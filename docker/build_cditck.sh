2f9b4a5f722f87145f0fab79f1f0a68fa9d1e476
ankitoracle <45846093+ankitoracle@users.noreply.github.com>
2019-08-23 16:21:45 +0530

    changes to integrate cdi tck 2.0.6 into cdi tck porting kit (#13)
    * changes for integrating cdi-tck-2.0.6
    
    Signed-off-by: Ankit <ankit.ku.jain@oracle.com>
    
    * changes to pom and build file
    
    Signed-off-by: Ankit <ankit.ku.jain@oracle.com>
    
    * corrected tck bundle url
    
    Signed-off-by: Ankit <ankit.ku.jain@oracle.com>
    
    * removing default value
    
    Signed-off-by: Ankit <ankit.ku.jain@oracle.com>

--- docker/build_cditck.sh
+++ docker/build_cditck.sh
@@ -20,22 +20,6 @@ echo "export MAVEN_HOME=$MAVEN_HOME"
 echo "export PATH=$PATH"
 
 cd $WORKSPACE
-WGET_PROPS="--progress=bar:force --no-cache"
-wget $WGET_PROPS $GF_BUNDLE_URL -O ${WORKSPACE}/latest-glassfish.zip
-unzip -o ${WORKSPACE}/latest-glassfish.zip -d ${WORKSPACE}
-
-which ant
-ant -version
-
-which mvn
-mvn -version
-
-
-sed -i "s#^porting\.home=.*#porting.home=$WORKSPACE#g" "$WORKSPACE/build.xml"
-sed -i "s#^glassfish\.home=.*#glassfish.home=$WORKSPACE/glassfish5/glassfish#g" "$WORKSPACE/build.xml"
-
-ant -version
-ant dist.sani
 
 mkdir -p ${WORKSPACE}/bundles
 if [ ! -z "$TCK_BUNDLE_BASE_URL" ]; then
@@ -44,6 +28,26 @@ if [ ! -z "$TCK_BUNDLE_BASE_URL" ]; then
   wget  --progress=bar:force --no-cache ${TCK_BUNDLE_BASE_URL}/${TCK_BUNDLE_FILE_NAME} -O ${WORKSPACE}/bundles/${TCK_BUNDLE_FILE_NAME}
   exit 0
 fi
+
+WGET_PROPS="--progress=bar:force --no-cache"
+wget $WGET_PROPS $GF_BUNDLE_URL -O ${WORKSPACE}/latest-glassfish.zip
+unzip -o ${WORKSPACE}/latest-glassfish.zip -d ${WORKSPACE}
+
+
+
+
+which ant
+ant -version
+
+which mvn
+mvn -version
+
+sed -i "s#^porting\.home=.*#porting.home=$WORKSPACE#g" "$WORKSPACE/build.xml"
+sed -i "s#^glassfish\.home=.*#glassfish.home=$WORKSPACE/glassfish5/glassfish#g" "$WORKSPACE/build.xml"
+
+ant -version
+ant dist.sani
+
 chmod 777 ${WORKSPACE}/dist/*.zip
 cd ${WORKSPACE}/dist/
 for entry in `ls cdi-tck-*.zip`; do
